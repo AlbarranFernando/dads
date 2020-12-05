4d7e55d1257c17ad638141c6d6d8a182b280b5bd
AlbarranFernando <69657806+AlbarranFernando@users.noreply.github.com>
2020-12-05 19:59:08 -0300

    Agregado de barra de navegacion

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './components/global/NavBar/NavBar';
+import NavBar from './components/global/NavBar/NavBar';
+//import logoDads from './assets/dona2.png';
+//<img src={logoDads} alt="logo dads drugstore"/>
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      
+    </>
+  );
+}
+
+
+export default App;
---
+++ src/components/global/NavBar/NavBar.jsx
@@ -0,0 +1,40 @@
+
+import './NavBar.css';
+function NavBar(){
+return (
+    <>
+        <header>
+            <div className="conHeader">
+                 <h1 className="titulo-principal">DADS DRUGSTORE</h1>
+                 <p className="subtitulo"  >Venta de Variedades</p>
+                    <nav>
+                        <ul>
+                            <li>
+                                <a href='#'>Golosinas</a>
+                            </li>
+                            <li>
+                                <a href='#'>Gaseosas</a>
+                            </li>
+                            <li>
+                                <a href='#'>Cervezas</a>
+                            </li>
+                            <li>
+                                <a href='#'>Almacen</a>
+                            </li>
+                            <li>
+                                <a href='#'>Variedades</a>
+                            </li>
+                        </ul>
+                    </nav>
+
+
+
+
+            </div>
+        </header>
+    </>
+)
+
+}
+
+export default NavBar;
